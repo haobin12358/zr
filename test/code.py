95013d4175f776d5ec270ce97d87594b4a8a0e9a
wktadmin <musicvae@gmail.com>
2018-09-08 23:47:31 +0800

    fix

--- test/code.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-headers = {
-    'Authorization': 'APPCODE a1ead5dd338d44c7ac4caa083a13278e'
-}
-url = 'http://mobai.market.alicloudapi.com/mobai_sms?param=code%3A9999&phone=18753391801&templateId=tp18090816'
-content = requests.get(url, )
